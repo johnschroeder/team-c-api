9524e770a766ea74ae3076b3c7337135531edb9d
elijah <emv2@pdx.edu>
2015-05-20 13:58:31 -0700

    IM-42

--- routes/displayInventory.js
+++ routes/displayInventory.js
@@ -1,23 +1,30 @@
 var mysql = require("mysql");
-var config = require("konfig")();
+var config = require("konfig");
 var express = require("express");
 var router = express.Router();
 
 
-router.route("/:prodID/:name/:customer:/description/:dateCreated:/RunID:/Amount").get(function(req,res){
+router.route("/").get(function(req,res){
 
-    var databaseName = "impDB";
+    var databaseName = "imp_db";
 
     /* To substitute into the query, if you want to add more tables, add them here and then insert them into query */
     var ProductTable = "Products";
     var RunTable = "Runs";
-    var BatchTable = "Batches"
+    var BatchTable = "Batches";
+
+    /* If you edit any tabels, add the fields here and it will change it in the query
+     * Also note that this should probably be moved to a module */
+    var ProdFields = "(ProductID int AUTO_INCREMENT, Name varchar(255), Customer varchar(255), Description varchar(255), DateCreated date, PRIMARY KEY (ProductID))";
+    var RunFields = "(RunID int AUTO_INCREMENT, ProductID int, Date date, PRIMARY KEY (RunID), FOREIGN KEY (ProductID) REFERENCES Products(ProductID))";
+    var BatchFields ="(RunID int, Amount float, Location VARCHAR(100), Foreign Key (RunID) References Runs(RunID))";
 
     /* Connects to mySQL server */
     var connection = mysql.createConnection({
-        host: config.app.mysql.host,
-        user: config.app.mysql.user,
-        password: config.app.mysql.password
+        host: "localhost",
+        user: "elijahvarga",
+        password: "dev.db124"
+
     });
 
     /* to figure out what queryInput is, look at queryExecute function, queryInput is the incoming argumentargument */
@@ -37,8 +44,12 @@ router.route("/:prodID/:name/:customer:/description/:dateCreated:/RunID:/Amount"
                             if (err) {
                                 console.error(err.stack);
                                 res.status(503).send("Commit Error: " + err.code);
-                            } else {
-                                res.send("Success!");
+                            }
+                            /*here is where the result of the query is happening
+                             * res.send is what we use to send things to the server it is like the return of a function */
+                            else {
+                                var invUnit = JSON.stringify(result);
+                                res.send(invUnit);
                                 connection.end();
                             }
                         });
@@ -49,15 +60,17 @@ router.route("/:prodID/:name/:customer:/description/:dateCreated:/RunID:/Amount"
     };
 
     var queryToExecute = queryFunction("USE " + databaseName,
-        queryFunction("SELECT * FROM"
-            + BatchTable + ", "
-            + RunTable   + ", "
-            + ProductTable + " "
-            + "WHERE "
-            + ProductTable +".ProductID =" + RunTable + ".RunID"
-            + "AND "
-            + RunTable + ".RunID = " + BatchTable + ".RunID"
-            + "GROUP BY" +ProductTable + "ProductID," + RunTable + ".RunID"
+        queryFunction("CREATE TABLE IF NOT EXISTS " + BatchTable + " " + BatchFields + ";",
+            queryFunction("CREATE TABLE IF NOT EXISTS " + ProductTable + " " + ProdFields,
+                queryFunction("CREATE TABLE IF NOT EXISTS " + RunTable + " " + RunFields,
+                   queryFunction("SELECT * FROM "
+                    + BatchTable + " NATURAL JOIN "
+                    + RunTable   + " NATURAL JOIN "
+                    + ProductTable + " "
+                    + "GROUP BY " + " ProductID, " + "RunID;"
+                    )
+                )
+            )
         )
     );
 
