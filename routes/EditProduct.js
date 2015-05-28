f9d5a1d4bd44697dcda4561d2d5264933a72ec57
elijah <emv2@pdx.edu>
2015-05-28 15:47:58 -0700

    IM-55

---
+++ routes/EditProduct.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var router = express.Router();
+var Q = require('q');
+
+
+router.route("/").get(function(req,res){
+    Q.longStackSupport = true;
+    var db = require("../imp_services/impdb.js").connect();
+    Q.fcall(db.beginTransaction())
+        .then(db.query("USE " + db.databaseName))
+        .then(db.query("CREATE TABLE IF NOT EXISTS " + db.productTable + " " + db.productFields + ";"))
+        .then(db.query("SELECT * FROM "
+        + db.productTable + " "
+        + "GROUP BY " + " ProductID; "))
+        .then(function(rows, columns){
+            console.log("Success");
+            var invUnit = JSON.stringify(rows[0]);
+            res.send(invUnit);
+            db.endTransaction();
+        })
+        .catch(function(err){
+            Q.fcall(db.rollback())
+                .then(db.endTransaction());
+            console.log("Error:");
+            console.error(err.stack);
+            res.status(503).send("ERROR: " + err.code);
+        })
+        .done();
+});
+
+module.exports = router;
