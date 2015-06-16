824ac8720df1e416b6272b9b0cdbd946bb7a53bf
kunmmckeighan <kunmckeighan@gmail.com>
2015-06-15 22:29:16 -0700

    IM-68 Add　quantity columns were changed to unsigned in Run table. More test data. finished stored procedure AddItemToCart.

--- config/StoredProcedures/AddItemToCart.sql
+++ config/StoredProcedures/AddItemToCart.sql
@@ -1,17 +1,50 @@
 
+/*
+To test this stored procedure, use below
+#####success test####
+set @m='';
+call AddItemtoCart(0,1,2,501,@m);
+select @m;
+#####not enough inventory test####
+set @m='';
+call AddItemtoCart(0,1,200000,501,@m);
+select @m;
+*/
+
 use dev_ImpDB;
-DROP PROCEDURE IF EXISTS AddItemToCart;
+DROP PROCEDURE IF EXISTS AddItemtoCart;
 
 DELIMITER $$
-CREATE PROCEDURE AddItemToCart
-(IN CartID int, IN SizeMapID int, IN Quantity int, IN RunID int, OUT msg varchar(256))
+CREATE PROCEDURE AddItemtoCart
+(IN _CartID int, IN _SizeMapID int, IN _Quantity int, IN _RunID int, OUT _Msg varchar(512))
 BEGIN
 
-DECLARE ProductID INT;
-SELECT @ProductID:=sm.ProductID
+mylabel: BEGIN
+DECLARE pdtID INT;
+DECLARE sz INT;
+SELECT @pdtID:=sm.ProductID,@sz:=sm.Size
 FROM SizeMap sm
-WHERE sm.RunID = RunID;
+WHERE sm.SizeMapID = _SizeMapID;
 
+set @ttlCount=@sz*_Quantity;
+select @avlb:=Runs.QuantityAvailable from Runs where Runs.RunID = _RunID;
+
+IF @avlb>=@ttlCount THEN
+	UPDATE Runs r
+		SET r.QuantityAvailable=r.QuantityAvailable-@ttlCount,
+		r.QuantityReserved= r.QuantityReserved+@ttlCount
+		WHERE RunID=_RunID;
+	SET _Msg = 'Success';
+ELSE
+	SET _Msg = 'Not enough inventory in the run';
+    SELECT _Msg;
+    LEAVE mylabel;
+END IF;
+
+INSERT INTO CartItems values(null,_CartID,_SizeMapID,_Quantity,_RunID);
+SELECT _Msg;
+
+END;
 END $$
 
 DELIMITER ;
--- config/TestData.sql
+++ config/TestData.sql
@@ -1,4 +1,4 @@
-use dev_impdb;
+use dev_ImpDB;
 insert into permissions values(null, 5);
 insert into users values("test01","bla","blalast", "sdfdsf","601", "1234","salt","2015-01-01");
 
@@ -7,3 +7,4 @@ insert into piles values(null, 101, "warehouse01");
 insert into runs values(null,301,"2015-01-03",500,0);
 
 insert into sizemap values(null,101,"package",50);
+insert into cart values(null,"cart1","test01","a1","2015-02-01", "2015-04-01");
