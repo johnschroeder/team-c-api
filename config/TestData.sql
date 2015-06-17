11e1422d21a2d405fad752ed8874c33556c73443
johnschroeder <john.schroeder4@gmail.com>
2015-06-17 16:35:27 -0700

    IM-68 capitalization stuff

--- config/TestData.sql
+++ config/TestData.sql
@@ -8,9 +8,9 @@ insert into Products values(null, "paper", "paper des", "2015-01-01");
 insert into Piles values(null, 101, "warehouse01");
 insert into Runs values(null,301,"2015-01-03",500,0);
 
-insert into Sizemap values(null,101,"package",50);
+insert into SizeMap values(null,101,"package",50);
 insert into Cart values(null,"cart1","test01","a1","2015-02-01", "2015-04-01");
 
-insert into Usergroups values("a1","test01");
-insert into Usergroups values("a1","test02");
+insert into UserGroups values("a1","test01");
+insert into UserGroups values("a1","test02");
 insert into Cart values(null,"cart2","test01","test01","2015-02-01", "2015-04-01");
