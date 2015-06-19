11ade42fe8688dc6666f376f0dab6986467c08e6
kunmmckeighan <kunmckeighan@gmail.com>
2015-06-18 18:11:13 -0700

    IM - 75 GetInventoryByProductID.sql added

---
+++ config/StoredProcedures/GetInventoryByProductID.sql
@@ -0,0 +1,15 @@
+use imp_db_dev;
+DROP PROCEDURE IF EXISTS GetInventoryByProductID;
+
+DELIMITER $$
+CREATE PROCEDURE GetInventoryByProductID(IN _ProductID int)
+BEGIN
+select  pl.PileID, pl.ProductID, sm.SizeMapID, sm.Name as SizeName, sm.Size, r.QuantityAvailable, pl.Location
+from Runs r
+left join Piles pl on r.PileID = pl.PileID
+left join SizeMap sm on sm.ProductID = pl.ProductID
+where pl.ProductID=_ProductID;
+
+END $$
+DELIMITER ;
+
