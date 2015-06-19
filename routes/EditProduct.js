dd02e2bdfcb79d1169b96f5541ca69dd3633ffcb
TimSwart <tswart@pdx.edu>
2015-06-19 14:31:46 -0700

    updated EditProduct.js

--- routes/EditProduct.js
+++ routes/EditProduct.js
@@ -2,23 +2,15 @@ var express = require("express");
 var router = express.Router();
 var Q = require('q');
 
-var querys =[
-    "Select a.ProductID, a.Description, c.Name FROM Products a, Customers c WHERE a.CustomerID = c.CustomerID"
-]
-
-
-router.route("/:selectedID").get(function(req,res){
+router.route("/:productID").get(function(req,res){
     Q.longStackSupport = true;
     var db = require("../imp_services/impdb.js").connect();
-    var selectedID  = req.params.selectedID;
     Q.fcall(db.beginTransaction())
         .then(db.query("USE " + db.databaseName))
-        //.then(db.query(querys))
-        .then(db.query("CREATE TABLE IF NOT EXISTS " + db.productTable + " " + db.productFields + ";"))
         .then(db.query("SELECT * FROM "
         + db.productTable + " "
-        + "WHERE " +selectedID + " = ProductID; "))
-        .then(function(rows, columns){
+        + "WHERE ProductID = " + req.params.productId))
+        .then(function(rows){
             console.log("Success");
             var invUnit = JSON.stringify(rows[0]);
             res.send(invUnit);
