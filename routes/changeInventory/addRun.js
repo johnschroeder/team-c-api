46bb2bbc7b99e4ac7b8d3c99d4d77d41b708d6cb
TimSwart <tswart@pdx.edu>
2015-06-19 14:35:41 -0700

    updated addRun.js

--- routes/changeInventory/addRun.js
+++ routes/changeInventory/addRun.js
@@ -6,18 +6,18 @@ var L = require('../../imp_services/logging.js');
 
 /*
  Usage:
- addRun - localhost:50001/changeInventory/addRun/productId/runDate
+ localhost:50001/changeInventory/addRun/productId/pileId/runDate/quantityAvailable
  */
 
-router.route("/:productId/:runDate").get(function(req, res) {
+router.route("/:productId/:pileId/:runDate/:quantAvail").get(function(req, res) {
     var db = require("../../imp_services/impdb.js").connect();
 
     //Q.longStackSupport = true;   // for error checking
 
     Q.fcall(db.beginTransaction())
         .then(db.query("USE " + db.databaseName))
-        .then(db.query("INSERT INTO " + db.runTable + " Values " + "(NULL, " + req.params.productId + ", " + req.params.runDate + ") "))
-        .then(L.updateLog(db, L.LOGTYPES.ADD.value, req.params.productId, null, null, null))
+        .then(db.query("INSERT INTO " + db.runTable + " Values " + "(NULL, " + req.params.pileId + ", '" + req.params.runDate + "', " + req.params.quantAvail + ", " + "0" + ") "))
+        .then(L.updateLog(db, L.LOGTYPES.ADDRUN.value, req.params.productId, null, req.params.quantAvail))
         .then(db.commit())
         .then(db.endTransaction())
         .then(function(){
