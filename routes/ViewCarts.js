d03f4b1474fc50c7dd2eefd62bc3adb47d13b1ce
elijah <emv2@pdx.edu>
2015-06-22 08:23:35 -0700

    IM-77

---
+++ routes/ViewCarts.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var router = express.Router();
+var Q = require('q');
+
+
+router.route("/").get(function(req,res){
+    Q.longStackSupport = true;
+    var db = require("../imp_services/impdb.js").connect();
+    Q.fcall(db.beginTransaction())
+        .then(db.query("USE " + db.databaseName))
+        .then(db.query("CALL GetAllCarts"))
+        .then(function(rows, columns){
+            console.log("Success");
+            var invUnit = JSON.stringify(rows[0][0]);
+            console.log("invUnit: " + invUnit);
+            res.send(invUnit);
+            db.endTransaction();
+        })
+        .catch(function(err){
+            Q.fcall(db.rollback())
+                .then(db.endTransaction());
+            console.log("Error:");
+            console.error(err.stack);
+            res.status(503).send("ERROR: " + err.code);
+        })
+        .done();
+});
+
+module.exports = router;
