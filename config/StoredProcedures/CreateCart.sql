8957484554bd9b1b15835d9c585e5050802fbbec
kunmmckeighan <kunmckeighan@gmail.com>
2015-06-24 16:18:38 -0700

    IM - 75 create stored procedure CreateCart and change the CreateCart route

--- config/StoredProcedures/CreateCart.sql
+++ config/StoredProcedures/CreateCart.sql
@@ -4,10 +4,11 @@ DROP PROCEDURE IF EXISTS CreateCart;
 
 DELIMITER $$
 CREATE PROCEDURE CreateCart
-(IN _CartName varchar(40), IN _Reporter varchar(25),IN _Assignee varchar(25),IN _KeepDays INT)
+(IN _CartName varchar(40), IN _Reporter varchar(25),IN _Assignee varchar(25),IN _KeepDays INT unsigned)
 BEGIN
 
-
+insert into Cart values
+(null, _CartName,_Reporter,_Assignee,CURDATE(), DATE_ADD(CURDATE(), INTERVAL _KeepDays DAY));
 
 END $$
 
