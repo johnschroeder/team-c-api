01a14fff1d81b549f146a1908c9949d2b03c8251
johnschroeder <john.schroeder4@gmail.com>
2015-06-25 00:06:11 -0700

    IM-79 needed a new backend route so deleting and adding would be in one transaction in case of error between them

--- config/StoredProcedures/GetCartItems.sql
+++ config/StoredProcedures/GetCartItems.sql
@@ -12,7 +12,7 @@ BEGIN
 SELECT ci.CartItemID, sm.ProductID, p.Name as ProductName,
 		pl.PileID, pl.Location, sm.SizeMapID, sm.Name as SizeName, sm.Size as CountPerBatch, ci.Quantity as BatchCount,
         sm.Size * ci.Quantity as Total
-FROM cartitems ci
+FROM CartItems ci
 NATURAL JOIN Runs r
 NATURAL JOIN Piles pl
 NATURAL JOIN SizeMap sm
--- routes/Carts/EditCartItem.js
+++ routes/Carts/EditCartItem.js
@@ -1,5 +1,5 @@
 /**
- * Created by Kun on 6/16/2015.
+ * Created by John Schroeder on 6/24/2015.
  */
 
 var mySQL = require("mysql");
@@ -8,13 +8,14 @@ var router = express.Router();
 var Q = require('q');
 /*
  Usage:
- localhost:50001/Carts/GetCartItems/{CartID}
- {CartID}: which cart's items do you want to diplay
-
- return table column names
- CartItemID, ProductID, ProductName, PileID, Location, SizeMapID, SizeName, CountPerBatch, BatchCount, Total, id
+ localhost:50001/Carts/EditCartItem/{CartID}/{CartItemID}/{SizeMapID}/{Quantity}/{RunID}
+ {CartItemID}: which cart item do you want to edit
+ {CartID}: cart to add back to
+ {SizeMapID}: new value
+ {Quantity}: new value
+ {RunID}: new value
  */
-router.route("/:CartID").get(function(req, res) {
+router.route("/:CartID/:CartItemID/:SizeMapID/:Quantity/:RunID").get(function(req, res) {
 
     //Q.longStackSupport = true;
     var db = require("../../imp_services/impdb.js").connect();
@@ -25,7 +26,13 @@ router.route("/:CartID").get(function(req, res) {
     var CartID = req.params.CartID;
     Q.fcall(db.beginTransaction())
         .then(db.query("USE " + db.databaseName))
-        .then(db.query("CALL " + db.spGetCartItems + "(" + CartID + ");"))
+        .then(db.query("CALL " + db.spDeleteItemInCart + "( "+ CartItemID +","+"@m" +")"))
+        .then(db.query("CALL " + db.spAddItemToCart + "("
+            + CartID + ", "
+            + SizeMapID + ", "
+            + Quantity + ", "
+            + RunID + "," + "@m);"
+        ))
         .then(function(rows, columns){
             console.log("Success");
             console.log(JSON.stringify(rows));
