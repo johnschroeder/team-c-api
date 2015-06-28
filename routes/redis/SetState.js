bc052d130233e3f618d8f0a92c572903b2315963
kunmmckeighan <kunmckeighan@gmail.com>
2015-06-28 15:28:59 -0700

    IM-87 session state: key=cookie, value=hashset(Username:username,Page:page)

--- routes/redis/SetState.js
+++ routes/redis/SetState.js
@@ -9,7 +9,7 @@ var router = express.Router();
 
 
 router.get('/redis/SetState/:cookie/:username/:page', function(req, res) {
-    client.hset(req.params.cookie,"Username",req.params.username,"Page" ,reg.params.page,function (error, result) {
+    client.hmset(req.params.cookie,"Username",req.params.username,"Page" ,reg.params.page,function (error, result) {
         if (error !== null) {
             console.log("error: " + error);
             res.send("error: " + error);
