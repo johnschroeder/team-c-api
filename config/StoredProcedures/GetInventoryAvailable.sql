2bb356cedbcfe854d83d701ffbac9f88ce53ec2c
TimSwart <tswart@pdx.edu>
2015-07-01 11:14:45 -0700

    added GetInventoryAvailable procedure

---
+++ config/StoredProcedures/GetInventoryAvailable.sql
@@ -0,0 +1,13 @@
+DROP PROCEDURE IF EXISTS GetInventoryAvailable;
+
+DELIMITER $$
+CREATE PROCEDURE GetInventoryAvailable
+(IN _ProductID int unsigned, IN _PileLocation varchar(50), IN _RunMarker varchar(30))
+BEGIN
+
+SELECT SUM(R.QuantityAvailable) AS TotalQuantity
+FROM Products Pr JOIN Piles Pi ON Pr.ProductID = Pi.ProductID JOIN Runs R ON Pi.PileID = R.PileID JOIN RunMarkers RM ON R.RunID = RM.RunID
+WHERE Pr.ProductID = _ProductID  AND Pi.Location = _PileLocation And RM.Marker = _RunMarker;
+
+END $$
+DELIMITER ;
