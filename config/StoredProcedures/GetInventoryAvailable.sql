117ca10511349407300380d4be8cee8bafd7acc9
TimSwart <tswart@pdx.edu>
2015-07-01 12:05:56 -0700

    added route for GetInventoryAvailable procedure

--- config/StoredProcedures/GetInventoryAvailable.sql
+++ config/StoredProcedures/GetInventoryAvailable.sql
@@ -5,7 +5,7 @@ CREATE PROCEDURE GetInventoryAvailable
 (IN _ProductID int unsigned, IN _PileLocation varchar(50), IN _RunMarker varchar(30))
 BEGIN
 
-SELECT SUM(R.QuantityAvailable) AS TotalQuantity
+SELECT SUM(R.QuantityAvailable) AS TotalQuantityAvailable
 FROM Products Pr JOIN Piles Pi ON Pr.ProductID = Pi.ProductID JOIN Runs R ON Pi.PileID = R.PileID JOIN RunMarkers RM ON R.RunID = RM.RunID
 WHERE Pr.ProductID = _ProductID  AND Pi.Location = _PileLocation And RM.Marker = _RunMarker;
 
