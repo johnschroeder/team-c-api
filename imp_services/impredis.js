90954220979da522723d044d70e6c2ce35320076
Michael Valentine <Michael_Valentine@mentor.com>
2015-07-01 21:11:31 -0700

    Correctly starting and stoping the redis client

--- imp_services/impredis.js
+++ imp_services/impredis.js
@@ -2,29 +2,35 @@ var redis = require('redis');
 var config = require("konfig")();
 var port=config.app.redis.port;
 var host=config.app.redis.host;
-var client = redis.createClient(port,host);
+
 
 module.exports = {
     get: function(cookie, callback) {
+        var client = redis.createClient(port,host);
         client.hgetall(cookie, function (error, val) {
             if (error !== null) {
                 console.log("error: " + error);
+                client.quit();
                 callback(null, error);
             }
             else {
                 console.log(val);
+                client.quit();
                 callback(val, null);
             }
         });
     },
     set: function(cookie, username, stateObject, callback){
+        var client = redis.createClient(port,host);
         client.hmset(cookie,"Username",username,"stateObject" ,stateObject,function (error, result) {
             if (error !== null) {
                 console.log("error: " + error);
+                client.quit();
                 callback(null, error)
             }
             else {
                 console.log("Success");
+                client.quit();
                 callback(result, null);
             }
         });
