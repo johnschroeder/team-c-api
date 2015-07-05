fafaffaef6dbc2aaec561216665ab07013c0e455
TimSwart <tswart@pdx.edu>
2015-07-05 16:00:05 -0700

    added route for AddLogViewMapEntry procedure

---
+++ routes/AddLogViewMapEntry.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var Q = require('q');
+var router = express.Router();
+
+
+/*
+ Usage:
+ localhost:50001/addLogViewMapEntry/Username/LogID
+ */
+
+router.route("/:username/:logId").get(function(req, res) {
+    var db = require("../imp_services/impdb.js").connect();
+
+    //Q.longStackSupport = true;   // for error checking
+
+    Q.fcall(db.beginTransaction())
+        .then(db.query("USE " + db.databaseName))
+        .then(db.query("CALL AddLogViewMapEntry (" + req.params.username + ", '" + req.params.logId + ")"))
+        .then(db.commit())
+        .then(db.endTransaction())
+        .then(function(rows){
+            var queryResult = JSON.stringify(rows[0][0]);
+            console.log("Success:   " + queryResult);
+            res.send(queryResult);
+        })
+
+        .catch(function(err){
+            Q.fcall(db.rollback())
+                .then(db.endTransaction())
+                .done();
+            console.log("Error: " + err);
+            //console.error(err.stack);
+            res.status(503).send("ERROR: " + err);
+        })
+        .done();
+});
+
+module.exports = router;
