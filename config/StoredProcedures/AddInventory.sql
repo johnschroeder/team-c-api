a51f3dca443801885fe148e05613980769473885
TimSwart <tswart@pdx.edu>
2015-07-06 18:09:40 -0700

    updated AddInventory procedure

--- config/StoredProcedures/AddInventory.sql
+++ config/StoredProcedures/AddInventory.sql
@@ -1,20 +1,17 @@
-
-use imp_db_dev;
 DROP PROCEDURE IF EXISTS AddInventory;
 
 DELIMITER $$
 CREATE PROCEDURE AddInventory
-(IN _ProductID int, IN _Quantity int,IN _Location varchar(50))
+(IN _ProductID int unsigned, IN _Quantity int unsigned, IN _Location varchar(50))
 BEGIN
+
 DECLARE plid INT;
 
-insert into Piles values(null, _ProductID, _Location);
+INSERT INTO Piles
+VALUES (null, _ProductID, _Location);
 
-select @plid:=max(PileID) from Piles
-where ProductID=_ProductID and Location=_Location;
-#
-insert into Runs values(null,@plid, CURDATE(),_Quantity,_Quantity,0);
+INSERT INTO Runs
+VALUES (null, last_insert_id(), CURDATE(), _Quantity, _Quantity, 0);
 
 END $$
-
 DELIMITER ;
