ff19dd43564d187a1513365839daa37082f427cc
TimSwart <tswart@pdx.edu>
2015-07-06 19:15:09 -0700

    updated CreateCart procedure

--- config/StoredProcedures/CreateCart.sql
+++ config/StoredProcedures/CreateCart.sql
@@ -1,15 +1,12 @@
-
-use imp_db_dev;
 DROP PROCEDURE IF EXISTS CreateCart;
 
 DELIMITER $$
 CREATE PROCEDURE CreateCart
-(IN _CartName varchar(40), IN _Reporter varchar(25),IN _Assignee varchar(25),IN _KeepDays INT unsigned)
+(IN _CartName varchar(40), IN _Reporter varchar(25), IN _Assignee varchar(25), IN _KeepDays int unsigned)
 BEGIN
 
-insert into Cart values
-(null, _CartName,_Reporter,_Assignee,CURDATE(), DATE_ADD(CURDATE(), INTERVAL _KeepDays DAY));
+INSERT INTO Cart
+VALUES (null, _CartName, _Reporter, _Assignee, CURDATE(), DATE_ADD(CURDATE(), INTERVAL _KeepDays DAY));
 
 END $$
-
 DELIMITER ;
