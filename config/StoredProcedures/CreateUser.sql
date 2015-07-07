beca8e4ac2ff89942b979b827120ee4aea2d3163
TimSwart <tswart@pdx.edu>
2015-07-06 19:26:04 -0700

    updated CreateUser procedure

--- config/StoredProcedures/CreateUser.sql
+++ config/StoredProcedures/CreateUser.sql
@@ -1,18 +1,18 @@
-
-use imp_db_dev;
 DROP PROCEDURE IF EXISTS CreateUser;
 
 DELIMITER $$
 CREATE PROCEDURE CreateUser
 (IN _Username varchar(25), IN _HP varchar(100), IN _Email varchar(255), IN _US varchar(50),
-IN _FirstName varchar(100),IN _LastName varchar(100),IN _DateCreated date)
+ IN _FirstName varchar(100), IN _LastName varchar(100), IN _DateCreated date)
 BEGIN
 
-DECLARE pmid INT;
-select @pmid:=min(PermsID) from Permissions;
+DECLARE pmid int unsigned;
 
-INSERT INTO Users VALUES(_Username,_FirstName,_LastName,_Email,@pmid,_HP,_US,_DateCreated);
+SELECT @pmid:=PermsID, MIN(Perms)
+FROM Permissions;
 
+INSERT INTO Users
+VALUES (_Username, _FirstName, _LastName, _Email, @pmid, _HP, _US, _DateCreated, default);
 
 END $$
 DELIMITER ;
