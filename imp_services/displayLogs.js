d1e22f83033639d24cad0fc5fc7579bce92f8ba8
Trevor <rtrevor@pdx.edu>
2015-07-07 16:21:35 -0700

    username lookup refactored.

--- imp_services/displayLogs.js
+++ imp_services/displayLogs.js
@@ -2,6 +2,7 @@
  * Created by Trevor on 7/3/2015.
  */
 var Q = require("q");
+var impredis = require("./impredis.js");
 
 /* var a = {};
  a["key1"] = "value1";
@@ -29,60 +30,66 @@ function toStringDefault (LogType, logUsername,  time,  actionData) {
 
 
 module.exports =
-
 {
-    _verifyKey :function (key) {
+    _verifyKey: function (key) {
         return LogTypeMap[key] == undefined ? false : true;
     },
 
-    displayLogs: function (username, callback) {
+    displayLogs: function (cookie, callback) {
+
         var db = require("../imp_services/impdb.js").connect();
         //var defer = Q.defer();
-       return Q.fcall(db.beginTransaction())
-            .then(db.query("USE " + db.databaseName))
-            .then(db.query("CALL GetLogsUserView(\'" + username + "\');"))
-            .then(function (rows) {
 
-                // We got data about the user
-                if (rows[0][0].length == 0) { // No user by that username
-                    return "Invalid Result!";
-                }
+        require("../imp_services/impredis.js").get(cookie, function usernameReturn(val)
+        {
+            var username = val.username;
 
-                var jsonLogs = [];
-                //console.log("---- Now to show the rows ------");
-                for (var i = 0; i < rows[0][0].length; i++) {
-                    var row = rows[0][0][i];
-                    var logID = row.LogID;
-                    var LogType = row.LogType;
-                    var logUsername = row.Username;
-                    var time = row.Time;
-                    var actionData = row.ActionData;
-                    console.log(actionData);
-                    /*jsonLogs[i] = '{"logID":"' + logID + '", "logType":"' + LogType +
-                        '","username":"' + logUsername + '","time":"' + time + '","action":"' +'"' + actionData + '"' + '"}';
-                    */
+            return Q.fcall(db.beginTransaction())
+                .then(db.query("USE " + db.databaseName))
+                .then(db.query("CALL GetLogsUserView(\'" + username+ "\');"))
+                .then(function (rows) {
 
-                    stringLogs[i] = LogTypeMap[LogType].callFunction(LogType, logUsername,  time,  JSON.parse(actionData));
-                }
-
-                for (var j = 0; j < stringLogs.length; j++) {
-                    jsonString += '"' + stringLogs[j] + '"';
-                    if (j + 1 < stringLogs.length) {
-                        jsonString += ',';
+                    // We got data about the user
+                    if (rows[0][0].length == 0) { // No user by that username
+                        return "Invalid Result!";
                     }
-                }
-                jsonString += ']}';
-                //console.log(jsonString);
-               //console.log(module.exports._verifyKey(800));
-                //callback(jsonString);
 
-               callback(jsonString);
+                    var jsonLogs = [];
+                    //console.log("---- Now to show the rows ------");
+                    for (var i = 0; i < rows[0][0].length; i++) {
+                        var row = rows[0][0][i];
+                        var logID = row.LogID;
+                        var LogType = row.LogType;
+                        var logUsername = row.Username;
+                        var time = row.Time;
+                        var actionData = row.ActionData;
+                        //console.log(actionData);
+                        /*jsonLogs[i] = '{"logID":"' + logID + '", "logType":"' + LogType +
+                         '","username":"' + logUsername + '","time":"' + time + '","action":"' +'"' + actionData + '"' + '"}';
+                         */
+                        stringLogs[i] = LogTypeMap[LogType].callFunction(LogType, logUsername, time, JSON.parse(actionData));
+                        console.log(stringLogs[i]);
+                    }
 
-            }) .then(db.endTransaction()) // This is called right?
-            .catch(function (err) {
-                Q.fcall().then(db.endTransaction())
-                    .then(console.log("We had an error"))
-                console.log("Error: " + err);
-            }).done();
+                    for (var j = 0; j < stringLogs.length; j++) {
+                        jsonString += '"' + stringLogs[j] + '"';
+                        if (j + 1 < stringLogs.length) {
+                            jsonString += ',';
+                        }
+                    }
+                    jsonString += ']}';
+                    //console.log(jsonString);
+                    //console.log(module.exports._verifyKey(800));
+                    //callback(jsonString);
+
+                    callback(jsonString);
+
+                }).then(db.endTransaction()) // This is called right?
+                .catch(function (err) {
+                    Q.fcall().then(db.endTransaction())
+                        .then(console.log("We had an error"))
+                    console.log("Error: " + err);
+                }).done();
+        })
     }
 };
