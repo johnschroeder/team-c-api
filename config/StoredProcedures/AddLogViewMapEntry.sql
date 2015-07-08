a8c90402550e3fcbe17d99927a22499f275c1125
TimSwart <tswart@pdx.edu>
2015-07-08 16:53:30 -0700

    removed last edit, presented syntax error in MySQL

--- config/StoredProcedures/AddLogViewMapEntry.sql
+++ config/StoredProcedures/AddLogViewMapEntry.sql
@@ -49,16 +49,22 @@ IF flag = false THEN # no foreign key errors
 	INTO test;
 
 	IF test = 1 THEN
-		# entry already exists, do nothing
+		# entry already exists
+		# show existing entry
+		SELECT *
+		FROM LogViewMap
+		WHERE Username = _Username AND LogID = _LogID;
 	ELSE
-		# entry does not yet exist, add entry
+		# entry does not yet exist
+		# add entry
 		INSERT INTO LogViewMap
 		VALUES (_Username, _LogID);
+
+		# show new entry
+		SELECT *
+		FROM LogViewMap
+		WHERE Username = _Username AND LogID = _LogID;
 	END IF;
-	# show newly added or existing entry
-	SELECT *
-	FROM LogViewMap
-	WHERE Username = _Username AND LogID = _LogID;
 ELSE # foreign key errors
 	SELECT message;
 END IF;
