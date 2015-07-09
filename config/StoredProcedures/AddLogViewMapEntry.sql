30a82607dc4c4de06490223a04857f17eece1f68
TimSwart <tswart@pdx.edu>
2015-07-08 17:21:51 -0700

    cleaned up procedure

--- config/StoredProcedures/AddLogViewMapEntry.sql
+++ config/StoredProcedures/AddLogViewMapEntry.sql
@@ -5,7 +5,6 @@ CREATE PROCEDURE AddLogViewMapEntry
 (IN _Username varchar(25), IN _LogID int unsigned)
 BEGIN
 
-DECLARE test int;
 DECLARE message varchar(100);
 DECLARE flag boolean;
 
@@ -13,58 +12,29 @@ SET message = 'Error:';
 SET flag = false;
 
 # check if username exists
-SELECT EXISTS(
-	SELECT 1
-	FROM Users
-	WHERE Username = _Username)
-INTO test;
-
-IF test = 0 THEN
+IF NOT EXISTS (SELECT * FROM Users WHERE Username = _Username) THEN
 	SET flag = true;
 	SET message = CONCAT(message, ' Username: ', _Username, ' doesn\'t exist. ');
 END IF;
 
-SET test = 0;
-
 # check if logId exists
-SELECT EXISTS(
-	SELECT 1
-	FROM Logs
-	WHERE LogID = _LogID)
-INTO test;
-
-IF test = 0 THEN
+IF NOT EXISTS (SELECT * FROM Logs WHERE LogID = _LogID) THEN
 	SET flag = true;
 	SET message = CONCAT(message, ' LogID: ', _LogID, ' doesn\'t exist.');
 END IF;
 
-SET test = 0;
 
 IF flag = false THEN # no foreign key errors
 	# check if entry already exists
-	SELECT EXISTS(
-		SELECT 1
-		FROM LogViewMap
-		WHERE Username = _Username AND LogID = _LogID)
-	INTO test;
-
-	IF test = 1 THEN
-		# entry already exists
-		# show existing entry
-		SELECT *
-		FROM LogViewMap
-		WHERE Username = _Username AND LogID = _LogID;
-	ELSE
-		# entry does not yet exist
-		# add entry
+	IF NOT EXISTS (SELECT * FROM LogViewMap WHERE LogID = _LogID AND Username = _Username) THEN
+		# entry does not yet exist, add entry
 		INSERT INTO LogViewMap
 		VALUES (_Username, _LogID);
-
+	END IF;
 		# show new entry
 		SELECT *
 		FROM LogViewMap
 		WHERE Username = _Username AND LogID = _LogID;
-	END IF;
 ELSE # foreign key errors
 	SELECT message;
 END IF;
