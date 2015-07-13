75a1370c272bc8bcbbd043d966fa68d4140c5ce2
hinyunlee <hinyunlee@gmail.com>
2015-07-12 21:48:21 -0700

    IM-159-AddProductSize: Added logging for addProductSize

--- imp_services/displayLogs.js
+++ imp_services/displayLogs.js
@@ -7,15 +7,23 @@ LogTypeMap[100] = {
         return time + " - " + logUsername + ": " + "Added " + actionData.quantity + " units of product " + actionData.productId + " to location " + actionData.location;
     }
 };
+
+LogTypeMap[200] = {
+    type: "Added Product Size",
+    callFunction: function (LogType, logUsername,  time,  actionData) {
+        return time + " - " + logUsername + ": " + "Added product size " + actionData.sizeName + " sized " + actionData.size + " for product " + actionData.productId;
+    }
+};
+
 LogTypeMap[800] = {
     type: "Created User",
     callFunction: function (LogType, logUsername, time, actionData) {
         return time + " - " + logUsername + ": " + "Created new user " + actionData.value;
     }
 };
+
 LogTypeMap[900] = {
     type: "Logged In User",
-
     callFunction: function (LogType, logUsername,  time,  actionData) {
         return time + " - " + logUsername + ": " + actionData.user + " logged in";
     }
