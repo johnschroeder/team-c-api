606c6b7474bca5da5bf646dbdaf92b38a6fd658e
TimSwart <tswart@pdx.edu>
2015-07-13 12:17:33 -0700

    updated AddItemToCart route

--- routes/Carts/AddItemToCart.js
+++ routes/Carts/AddItemToCart.js
@@ -1,8 +1,3 @@
-/**
- * Created by Kun on 6/15/2015.
- */
-
-
 var mySQL = require("mysql");
 var express = require("express");
 var router = express.Router();
@@ -28,22 +23,6 @@ router.route("/:CartID/:SizeMapID/:Quantity/:RunID").get(function(req, res) {
     var SizeMapID = req.params.SizeMapID;
     var Quantity = req.params.Quantity;
     var RunID = req.params.RunID;
-    /**
-     * The initial use of Q.fcall() is required to kickstart the chain.
-     * Once that's done, the resulting object is a promise.
-     * The promise contains a .then() method, which takes as its first (and usually only)
-     * argument a function.
-     * If the promise is fulfilled:
-     *      There were no errors during the execution of the wrapped method
-     *      The then() method's argument is called
-     *          Arguments to the function inside then() are results of the previous promise!
-     *          (see rows, columns from below)
-     * If the promise was not fulfilled:
-     *      There was an error during the execution of the wrapped method
-     *      The error is passed on (or then's second argument is called)
-     *      The error is used as an argument to .catch()'s function.
-     * The .done() ends the chain.
-     */
     Q.fcall(db.beginTransaction())
         .then(db.query("USE " + db.databaseName))
         .then(db.query("set @m='';"))
@@ -53,13 +32,6 @@ router.route("/:CartID/:SizeMapID/:Quantity/:RunID").get(function(req, res) {
             + Quantity + ", "
             + RunID + "," + "@m);"
         ))
-    /**
-     * The args here are results of the previous promise's wrapped function.
-     * Note that I had to make the function in the then a promise by using the
-     * defer() method to allow for further chaining.  This part is only necessary
-     * if you need results from one of the wrapped method beyond success/failure.
-     * Uncomment the console log to see that it's accessing values from the table
-     */
         .then(function(rows, columns){
             var deferred = Q.defer();
             //console.log(rows);
@@ -81,6 +53,7 @@ router.route("/:CartID/:SizeMapID/:Quantity/:RunID").get(function(req, res) {
             res.status(503).send("ERROR: " + err.code);
 
         })
+        // TODO: hard to log anything meaningful here without retrieving more data from DB
         .done();
 });
 
