1821d938d03cca79b3c5e4a207d160d0fcc16220
TimSwart <TimSwart@users.noreply.github.com>
2015-07-21 16:07:43 -0700

    Update SingleSizeMapping.sql

--- config/Triggers/SingleSizeMapping.sql
+++ config/Triggers/SingleSizeMapping.sql
@@ -9,7 +9,8 @@ INSERT INTO SizeMap
 VALUES (null, (SELECT MAX(ProductID) FROM Products), 'single', 1);
 
 # MAX(ProductID) might not be quite as safe as using last_insert_id(), but
-# that doesn't work due to the insert being outside the scope of the trigger
+# that doesn't work due to the insert of the product being outside the
+# scope of the trigger
 
 END $$
 DELIMITER ;
