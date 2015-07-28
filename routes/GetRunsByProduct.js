f51fc59ccfb5eeac41faac624aa5a00f940d7ab2
TimSwart <tswart@pdx.edu>
2015-07-27 21:20:13 -0700

    added route for getting run data

---
+++ routes/GetRunsByProduct.js
@@ -0,0 +1,39 @@
+var mySQL = require("mysql");
+var express = require("express");
+var router = express.Router();
+var Q = require('q');
+/*
+ Usage:
+ localhost:50001/GetRunsByProduct/{ProductID}
+ This route returns all runs for a given productID
+ */
+router.route("/:ProductID").get(function(req, res) {
+
+    //Q.longStackSupport = true;
+    var db = require("../imp_services/impdb.js").connect();
+
+    /**
+     *  Package up some values from the route
+     */
+    var ProductID = req.params.ProductID;
+    Q.fcall(db.beginTransaction())
+        .then(db.query("USE " + db.databaseName))
+        .then(db.query("CALL GetRunsByProduct" + "(" + ProductID + ");"))
+        .then(function(rows){
+            console.log("Successfully got run data for product " + ProductID);
+            var invUnit = JSON.stringify(rows[0][0]);
+            res.send(invUnit);
+        })
+        .then(db.commit())
+        .then(db.endTransaction())
+        .catch(function(err){
+            Q.fcall(db.rollback())
+                .then(db.endTransaction());
+            console.log("Error:");
+            console.error(err.stack);
+            res.status(503).send("ERROR: " + err.code);
+        })
+        .done();
+});
+
+module.exports = router;
