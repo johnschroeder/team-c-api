7cb887e62bc68596f42d0ea37bee76cf0c92b587
TimSwart <tswart@pdx.edu>
2015-07-28 00:26:56 -0700

    added second version of addInventory to incorporate new additions without having to ruin DB for others in the meantime

---
+++ config/StoredProcedures/AddInventory2.sql
@@ -0,0 +1,30 @@
+DROP PROCEDURE IF EXISTS AddInventory2;
+
+DELIMITER $$
+CREATE PROCEDURE AddInventory2
+(IN _ProductID int unsigned, IN _Quantity int unsigned, IN _Location varchar(50), IN _AltID int unsigned)
+BEGIN
+
+DECLARE existingPileID int unsigned;
+SET existingPileID = null;
+
+# check if a pile already exists for that product and location
+Select PileID
+FROM Piles
+WHERE ProductID = _ProductID AND Location = _Location
+INTO existingPileID;
+
+IF (existingPileID IS NULL) THEN
+	# no pile exists, create new one
+	INSERT INTO Piles
+  	VALUES (null, _ProductID, _Location);
+
+ 	SET existingPileID = last_insert_id(); # get new pileId
+END IF;
+
+# create new run using either the existing pileId or a newly created pileId
+INSERT INTO Runs
+VALUES (null, existingPileID, CURDATE(), _Quantity, _Quantity, 0, _AltID);
+
+END $$
+DELIMITER ;
