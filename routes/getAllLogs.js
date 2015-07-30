d38099ce30e1d57e34e73e30f96225393f063730
Trevor <rtrevor@pdx.edu>
2015-07-30 15:50:29 -0700

    Almost Done splitting from 209

--- routes/getAllLogs.js
+++ routes/getAllLogs.js
@@ -2,10 +2,9 @@ var express = require("express");
 var router = express.Router();
 var Q = require('q');
 
-router.route('/:filterParameters').get(function(req,res) {
+router.route('/').get(function(req,res) {
     var logsService = require('../imp_services/displayLogs');
-
-    var filters = req.params.filterParameters;
+    
     logsService.displayLogs(true, filters, req.cookies.IMPId, function (logs) {
         res.end(logs);
     });
