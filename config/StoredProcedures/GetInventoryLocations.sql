092a56a38967b123c4c034e8f886bd5032ed999e
Trevor <rtrevor@pdx.edu>
2015-07-30 16:33:23 -0700

    Almost Done splitting from 209

--- config/StoredProcedures/GetInventoryLocations.sql
+++ config/StoredProcedures/GetInventoryLocations.sql
@@ -1,12 +1,14 @@
-DROP PROCEDURE IF EXISTS GetInventoryLocations;
+DROP PROCEDURE IF EXISTS imp_db_dev.GetInventoryLocations;
 
 DELIMITER $$
-CREATE PROCEDURE GetInventoryLocations
-()
+
+CREATE PROCEDURE GetInventoryLocations(_productID)
+
 BEGIN
 
-SELECT *
-FROM Location;
+SELECT DISTINCT(Piles.Location) FROM Piles
+JOIN Products ON Piles.ProductID = Products.ProductID
+WHERE Products.ProductID = _productID;
 
 END $$
 DELIMITER ;
