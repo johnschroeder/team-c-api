9b02b4b36959ff746347d709b46b539f1c8cf001
TimSwart <tswart@pdx.edu>
2015-07-30 12:51:15 -0700

    updated GetRunsByProductID SP again to perform ordering on backend

--- config/StoredProcedures/GetRunsByProductID.sql
+++ config/StoredProcedures/GetRunsByProductID.sql
@@ -7,7 +7,8 @@ BEGIN
 
 SELECT R.RunID, IFNULL(R.AltID, 'n/a') AS AltID, Pi.Location, R.DateCreated, R.InitialQuantity, R.QuantityAvailable, R.QuantityReserved, IFNULL(RM.Marker, 'none') AS Marker
 FROM Products Pr JOIN Piles Pi ON Pr.ProductID = Pi.ProductID JOIN Runs R ON Pi.PileID = R.PileID LEFT JOIN RunMarkers RM ON R.RunID = RM.RunID
-WHERE Pr.ProductID = _ProductID;
+WHERE Pr.ProductID = _ProductID
+ORDER BY R.RunID DESC;
 
 END $$
 DELIMITER ;
