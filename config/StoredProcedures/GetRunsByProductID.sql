0f54bd4cec41ae873d8909ddadeb945f061c35cd
TimSwart <tswart@pdx.edu>
2015-07-30 12:17:06 -0700

    updated GetRunsByProductID SP again

--- config/StoredProcedures/GetRunsByProductID.sql
+++ config/StoredProcedures/GetRunsByProductID.sql
@@ -5,7 +5,7 @@ CREATE PROCEDURE GetRunsByProduct
 (IN _ProductID int unsigned)
 BEGIN
 
-SELECT R.RunID, R.AltID, Pi.Location, R.DateCreated, R.InitialQuantity, R.QuantityAvailable, R.QuantityReserved, IFNULL(RM.Marker, 'none') AS Marker
+SELECT R.RunID, IFNULL(R.AltID, 'n/a') AS AltID, Pi.Location, R.DateCreated, R.InitialQuantity, R.QuantityAvailable, R.QuantityReserved, IFNULL(RM.Marker, 'none') AS Marker
 FROM Products Pr JOIN Piles Pi ON Pr.ProductID = Pi.ProductID JOIN Runs R ON Pi.PileID = R.PileID LEFT JOIN RunMarkers RM ON R.RunID = RM.RunID
 WHERE Pr.ProductID = _ProductID;
 
