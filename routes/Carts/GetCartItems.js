f204ebaf72be0350f4ee759e3a8dda1ff0d77c75
TimSwart <tswart@pdx.edu>
2015-08-03 21:23:25 -0700

    IM-252: cleaned up GetCartItems route

--- routes/Carts/GetCartItems.js
+++ routes/Carts/GetCartItems.js
@@ -1,7 +1,3 @@
-/**
- * Created by Kun on 6/16/2015.
- */
-
 var mySQL = require("mysql");
 var express = require("express");
 var router = express.Router();
@@ -9,35 +5,28 @@ var Q = require('q');
 /*
  Usage:
  localhost:50001/Carts/GetCartItems/{CartID}
- {CartID}: which cart's items do you want to diplay
+ {CartID}: cartID of the cart you want to retrieve items for
+*/
 
- return table column names
- CartItemID, ProductID, ProductName, PileID, Location, SizeMapID, SizeName, CountPerBatch, BatchCount, Total, id
- */
 router.route("/:CartID").get(function(req, res) {
 
     //Q.longStackSupport = true;
     var db = require("../../imp_services/impdb.js").connect();
-
-    /**
-     *  Package up some values from the route
-     */
     var CartID = req.params.CartID;
+
     Q.fcall(db.beginTransaction())
         .then(db.query("USE " + db.databaseName))
         .then(db.query("CALL " + db.spGetCartItems + "('" + CartID + "');"))
-        .then(function(rows, columns){
-            console.log("Success");
-            //console.log(JSON.stringify(rows));
-            var invUnit = JSON.stringify(rows[0]);
-            res.send(invUnit);
+        .then(function (rows) {
+            var queryResult = rows[0];
+            res.send(queryResult);
         })
         .then(db.commit())
         .then(db.endTransaction())
-        .catch(function(err){
+        .catch(function (err) {
             Q.fcall(db.rollback())
                 .then(db.endTransaction());
-            console.log("Error:");
+            console.log("Error in GetCartItems route.");
             console.error(err.stack);
             res.status(503).send("ERROR: " + err.code);
         })
