ecdfe393a5850ec388f168a41a49a4280e0e1504
Trevor <rtrevor@pdx.edu>
2015-08-05 16:57:55 -0700

    "Request Delete" -> "Request Deactivate"

--- config/StoredProcedures/EditUserByUsername.sql
+++ config/StoredProcedures/EditUserByUsername.sql
@@ -1,18 +1,18 @@
 DROP PROCEDURE IF EXISTS EditUserByUsername;
 CREATE PROCEDURE EditUserByUsername
-(IN _username varchar(255), IN _firstname varchar(255), IN _lastname varchar(255), IN _permissions int, IN _requestDelete int)
+(IN _username varchar(255), IN _firstname varchar(255), IN _lastname varchar(255), IN _permissions int, IN _requestDeactivate int)
 
 DELIMITER $$
 
 BEGIN
 
-IF (_requestDelete = 1) THEN
+IF (_requestDeactivate = 1) THEN
 
 UPDATE Users
 SET FirstName = _firstname, LastName = _lastname, PermsID = _permissions, isConfirmed = 0
 WHERE Username = _username;
 
-ELSEIF (_requestDelete = 0) THEN
+ELSEIF (_requestDeactivate = 0) THEN
 
 UPDATE Users
 SET FirstName = _firstname, LastName = _lastname, PermsID = _permissions
